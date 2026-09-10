b5d7a8f400d712497edca12c324578e66760fac2
jacobjjo <jacobjjo@udel.edu>
2026-09-10 16:25:11 -0400

    Testing calculating geocoordinates from drag release event

--- map-project/src/App.tsx
+++ map-project/src/App.tsx
@@ -1,32 +1,14 @@
 import './App.css';
-import { useState, useRef } from 'react';
-import Draggable from './components/Draggable';
-import Map from './components/Map';
-import type { DraggableData } from 'react-rnd';
+import MapController from './components/MapController';
 
-
-interface DragStopEvent {
-  type: string;
-}
 
 function App() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const dropZoneRef = useRef<HTMLDivElement | null>(null);
-
-  const handleDragStop = (e: DragStopEvent, d: DraggableData): void => {
-    console.log('Drag stopped at:', d.x, d.y);
-    setPosition({ x: d.x, y: d.y });
-  };
-
+  
   return (
     <div className="App">
       <div id="map-container">
-        <div ref={dropZoneRef} id="map" style={{ height: '80vh', width: '100%' }}>
-          <Map width={1000} height={1000} />
-        </div>
-        <div id="draggable-container">
-          <Draggable position={position} onDragStop={handleDragStop} />
-        </div>
+        <h1>Delaware Map</h1>
+        <MapController />
       </div>
     </div>
   )
